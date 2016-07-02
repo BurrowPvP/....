132c5bd3a378c4b97c32fc648d27dacd37f9e485
PaulchenHD <paulchen_hd@web.de>
2016-07-02 06:53:42 -1100

    Update and rename tasks/CountdownTask.php to task/CountdownTask.php

---
+++ task/CountdownTask.php
@@ -0,0 +1,35 @@
+<?php
+/*
+   Copyright 2016 BurrowPvP
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+limitations under the License.
+*/
+namespace SuperJump\tasks;
+use pocketmine\scheduler\PluginTask;
+use Secret\Main;
+class CheckBlockUnderTask extends PluginTask{
+  /** @var Main */
+  private $plugin;
+  
+  public function __construct(Main $plugin){
+    parent::__construct($plugin);
+    $this->plugin = $plugin;
+  }
+  
+  public function onRun($currentTick){
+    foreach($this->plugin->getServer()->getPlayer($this->plugin->players) as $player){
+       $block = $player->getLevel()->getBlock($player->floor()->subtract(0, 1));
+       if($block->getId() === 1){
+          // do something
+       }
+    }
+  }
+  
+}
