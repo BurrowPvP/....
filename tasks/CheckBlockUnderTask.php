d0cf7b4f8ae944c21f68f3e26756b5d09fec522a
Survingo <thehydra.tk@gmail.com>
2016-07-02 19:00:54 +0200

    Update CheckBlockUnderTask.php

--- tasks/CheckBlockUnderTask.php
+++ tasks/CheckBlockUnderTask.php
@@ -3,5 +3,19 @@
 namespace Secret\tasks;
 
 use pocketmine\scheduler\PluginTask;
+use Secret\Main;
 
 class CheckBlockUnderTask extends PluginTask{
+  /** @var Main */
+  private $plugin;
+  
+  public function __construct(Main $plugin){
+    parent::__construct($plugin);
+    $this->plugin = $plugin;
+  }
+  
+  public function onRun($currentTick){
+    
+  }
+  
+}
